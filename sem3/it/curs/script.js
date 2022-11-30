c5bad3af6baa23f2783f049bea02c0f643b9ab23
bynik <alesha.bessonov2003@mail.ru>
2022-11-30 17:30:12 +0700

    курсовик по ит

---
+++ sem3/it/curs/script.js
@@ -0,0 +1,102 @@
+list1=document.querySelector('.list1');
+list2=document.querySelector('.list2');
+list3=document.querySelector('.list3');
+list4=document.querySelector('.list4');
+
+q = document.querySelector('.q');
+sum = document.querySelector('.sum');
+table = document.querySelector('table');
+n = 1;
+
+function show(element){
+    if(element.innerHTML == 'DeFi'){
+        list1.style.display = "block";
+        list2.style.display = "none";
+		list3.style.display = "none";
+		list4.style.display = "none";
+    }
+    else if(element.innerHTML == 'NFT')
+    {
+        list1.style.display = "none";
+        list2.style.display = "block";
+		list3.style.display = "none";
+		list4.style.display = "none";
+    }
+	else if(element.innerHTML == 'Metaverse')
+    {
+        list1.style.display = "none";
+        list2.style.display = "none";
+		list3.style.display = "block";
+		list4.style.display = "none";
+    }
+	else if(element.innerHTML == 'on the hype')
+    {
+        list1.style.display = "none";
+        list2.style.display = "none";
+		list3.style.display = "none";
+		list4.style.display = "block";
+    }
+}
+
+function summ(element) {
+	parent = element.parentElement;
+	p = parent.querySelector('p');
+    h3 = parent.querySelector('h3');
+	
+	tr = document.createElement('tr');
+	table.append(tr);
+	
+	td = document.createElement('td');
+	td.innerHTML = n;
+	tr.append(td);
+	
+
+	td = document.createElement('td');
+	td.innerHTML = p.innerHTML;
+	tr.append(td);
+	
+	td = document.createElement('td');
+	td.innerHTML = h3.innerHTML;
+	tr.append(td);
+	
+	// td = document.createElement('td');
+	// cross = document.createElement('img');
+	// cross.setAttribute('src', 'this');
+	// cross.setAttribute('width', '20px');
+	// cross.setAttribute('onClick', 'closes(this)');
+	// td.append(cross);
+	// tr.append(td);
+	
+	q.innerHTML = Number(q.innerHTML) + 1;
+	sum.innerHTML = Number(sum.innerHTML.replace(/[a-za-яё\s$]/gi,'')) + Number(h3.innerHTML.replace(/[a-za-яё\s$  ]/gi,''));
+	sum.innerHTML += '$'
+	n++;
+}
+
+chet = 0;
+
+function openspisok() {
+    if (chet == 0)
+    {
+        spisok = document.querySelector('.spisok');
+	    spisok.style.display = "block";
+        chet = 1;
+    }
+
+    else if (chet == 1)
+    {
+        spisok = document.querySelector('.spisok');
+	    spisok.style.display = "none";
+        chet = 0;
+    }
+	
+}
+
+function closes(element) {
+	parent = element.parentElement.parentElement;
+	parent.remove();
+    pa = p.element;
+    /*q.innerHTML = Number(q.innerHTML) - 1;*/
+    /*sum.innerHTML = Number(sum.innerHTML.replace(/[a-za-яё\s]/gi,'')) - Number(h3.innerHTML.replace(/[a-za-яё\s]/gi,''));*/
+   
+}
--- sem3/it/laba9/script.js
+++ sem3/it/laba9/script.js
@@ -100,21 +100,3 @@ function closes(element) {
     /*sum.innerHTML = Number(sum.innerHTML.replace(/[a-za-яё\s]/gi,'')) - Number(h3.innerHTML.replace(/[a-za-яё\s]/gi,''));*/
    
 }
-$(function() {
-	let header = $('.header');
-	let hederHeight = header.height(); // вычисляем высоту шапки
-	 
-	$(window).scroll(function() {
-	  if($(this).scrollTop() > 1) {
-	   header.addClass('header_fixed');
-	   $('body').css({
-		  'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
-	   });
-	  } else {
-	   header.removeClass('header_fixed');
-	   $('body').css({
-		'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
-	   })
-	  }
-	});
-   });
