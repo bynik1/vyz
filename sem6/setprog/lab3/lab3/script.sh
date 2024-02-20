fece7a90d872a81266689467f1d5fbd7685a381b
bynik <alesha.bessonov2003@mail.ru>
2024-02-20 15:48:57 +0700

    editing script.sh

--- sem6/setprog/lab3/lab3/script.sh
+++ sem6/setprog/lab3/lab3/script.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 # Создание log файлов, если они не существуют
-touch server.log
-touch client_output_1.log
-touch client_output_2.log
+touch server.log client_output_1.log client_output_2.log
 
-# Компиляция сервера
+# Компиляция сервера и клиентов
 gcc server.c -o server
+gcc client.c -o client1
+gcc client.c -o client2
 
-# Запуск сервера в новом терминале xterm и запись его вывода в файл server.log для последующего получения порта и IP
-xterm -e "bash -c ./server > server.log" &
+# Даем права на выполнение
+chmod +x server client1 client2
 
-# Задержка, чтобы сервер успел запуститься и получить порт
-sleep 2
+# Запуск сервера и клиентов в новых терминалах xterm
+xterm -hold -e "./server > server.log" &
+sleep 2  # Даем время серверу на запуск
 
-# Чтение порта и IP адреса из лога сервера
+# Извлекаем порт и IP (адаптируйте эти команды под формат вашего лога)
 port=$(grep "Listening on port" server.log | awk '{print $NF}')
 # IP адрес может потребоваться установить статически, если сервер слушает на localhost или 0.0.0.0
 ip=$(grep "IP адрес сервера" server.log | awk '{print $NF}') # Если ваш сервер выводит IP адрес, иначе установите статически
 
-# Компиляция клиентов
-gcc client.c -o client1
-gcc client.c -o client2
 
-# Запуск клиентов в новых терминалах xterm с передачей IP адреса и порта и перенаправлением их вывода в соответствующие log файлы
-xterm -e "bash -c ./client1 $ip $port> client_output_1.log" &
-xterm -e "bash -c ./client2 $ip $port> client_output_2.log" &
+# Запуск клиентских программ
+xterm -hold -e "./client1 $ip $port 10 > client_output_1.log" &
+xterm -hold -e "./client2 $ip $port 5 > client_output_2.log" &
