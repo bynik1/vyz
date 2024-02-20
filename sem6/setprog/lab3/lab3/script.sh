edc60a7e11c6e247f4ed3ffa9e1ae8be6759506b
bynik <alesha.bessonov2003@mail.ru>
2024-02-20 20:56:59 +0700

    editing script.sh

--- sem6/setprog/lab3/lab3/script.sh
+++ sem6/setprog/lab3/lab3/script.sh
@@ -28,5 +28,6 @@ ip=$(grep "IP адрес сервера" server.log | awk '{print $NF}')
 echo "IP: $ip, Port: $port"
 
 # Запуск клиентов в новых терминалах xterm с передачей IP адреса и порта и перенаправлением их вывода в соответствующие log файлы
-xterm -hold -e "bash -c './client1 $ip $port 10 > client_output_1.log; exec bash'" &
-xterm -hold -e "bash -c './client2 $ip $port 10 > client_output_2.log; exec bash'" &
+xterm -hold -e "bash -c './client1 \"$ip\" \"$port\" 10 > client_output_1.log 2>&1; exec bash'" &
+xterm -hold -e "bash -c './client2 \"$ip\" \"$port\" 10 > client_output_2.log 2>&1; exec bash'" &
+
